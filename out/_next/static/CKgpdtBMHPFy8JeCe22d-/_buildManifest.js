ba696e69df02a79fd1e014fbb62bfa3b4e8a0039
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-08-11 06:24:59 +0530

    fix build

---
+++ out/_next/static/CKgpdtBMHPFy8JeCe22d-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2445-851948163dae2594.js","static/chunks/pages/index-d646f8ef3d4c7939.js"],"/_error":["static/chunks/pages/_error-7891c9bfcd7b3e53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
